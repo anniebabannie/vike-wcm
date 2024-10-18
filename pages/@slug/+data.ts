550f94d3532ec03fa60b938c5f6ec74d3e6d5259
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-10-18 13:50:54 -0700

    WIP

---
+++ pages/@slug/+data.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { PageContextServer } from "vike/types"
+import type { Comic, Chapter, Page, Prisma } from "@prisma/client";
+import { getChapterPages } from "./@pageNo/+data";
+import { redirect } from "vike/abort";
+
+const data = async (pageContext: PageContextServer) => {
+  const prisma = new PrismaClient();
+  const currentChapter = await prisma.chapter.findUnique({
+    where: { slug: pageContext.routeParams.slug },
+    include: { pages: {
+      orderBy: { pageNo: 'asc'}
+    } }
+  });
+  
+  if (currentChapter && currentChapter.pages.length > 0) {
+    const firstPage = currentChapter.pages[0];
+    console.log(firstPage)
+    throw redirect(`/${currentChapter.slug}/${firstPage.pageNo}`);
+  }
+}
+
+export { data }
